ad2c8408aeb6f788bff184a1cc24fa54ae363460
skbrown333 <skbrown333@gmail.com>
2018-02-20 11:19:56 -0600

    Fix linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,32 @@
 
 module.exports = function (grunt) {
 
-  grunt.initConfig({
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          require: 'coffee-script'
-        },
-        src: ['test/**/*.js']
-      }
-    },
-    release: {
-      options: {
-        tagName: 'v<%= version %>',
-        commitMessage: 'Prepared to release <%= version %>.'
-      }
-    },
-    watch: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      tasks: ['test']
-    }
-  });
+	grunt.initConfig({
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					require: 'coffee-script'
+				},
+				src: ['test/**/*.js']
+			}
+		},
+		release: {
+			options: {
+				tagName: 'v<%= version %>',
+				commitMessage: 'Prepared to release <%= version %>.'
+			}
+		},
+		watch: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			tasks: ['test']
+		}
+	});
 
-  // load all grunt tasks
-  require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
+	// load all grunt tasks
+	require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
 
-  grunt.registerTask('test', ['mochaTest']);
-  grunt.registerTask('test:watch', ['watch']);
-  grunt.registerTask('default', ['test']);
+	grunt.registerTask('test', ['mochaTest']);
+	grunt.registerTask('test:watch', ['watch']);
+	grunt.registerTask('default', ['test']);
 };
